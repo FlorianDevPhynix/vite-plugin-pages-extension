e15aaf348e16d786bb7c00324d5e8b0685e777b1
Florian Stemmler <florian.stemmler@outlook.de>
2023-08-16 00:06:27 +0200

    Add prettier

--- src/context.ts
+++ src/context.ts
@@ -1,136 +1,146 @@
-import { join, resolve } from 'path'
-import { slash, toArray } from '@antfu/utils'
-import { resolveOptions } from './options'
-import { getPageFiles } from './files'
-import { debug, invalidatePagesModule, isTarget } from './utils'
+import { join, resolve } from 'path';
+import { slash, toArray } from '@antfu/utils';
+import { resolveOptions } from './options';
+import { getPageFiles } from './files';
+import { debug, invalidatePagesModule, isTarget } from './utils';
 
-import type { FSWatcher } from 'fs'
-import type { Logger, ViteDevServer } from 'vite'
-import type { PageOptions, ResolvedOptions, UserOptions } from './types'
+import type { FSWatcher } from 'fs';
+import type { Logger, ViteDevServer } from 'vite';
+import type { PageOptions, ResolvedOptions, UserOptions } from './types';
 
 export interface PageRoute {
-  path: string
-  route: string
+	path: string;
+	route: string;
 }
 
 export class PageContext {
-  private _server: ViteDevServer | undefined
-  private _pageRouteMap = new Map<string, PageRoute>()
+	private _server: ViteDevServer | undefined;
+	private _pageRouteMap = new Map<string, PageRoute>();
 
-  rawOptions: UserOptions
-  root: string
-  options: ResolvedOptions
-  logger?: Logger
+	rawOptions: UserOptions;
+	root: string;
+	options: ResolvedOptions;
+	logger?: Logger;
 
-  constructor(userOptions: UserOptions, viteRoot: string = process.cwd()) {
-    this.rawOptions = userOptions
-    this.root = slash(viteRoot)
-    debug.env('root', this.root)
-    this.options = resolveOptions(userOptions, this.root)
-    debug.options(this.options)
-  }
+	constructor(userOptions: UserOptions, viteRoot: string = process.cwd()) {
+		this.rawOptions = userOptions;
+		this.root = slash(viteRoot);
+		debug.env('root', this.root);
+		this.options = resolveOptions(userOptions, this.root);
+		debug.options(this.options);
+	}
 
-  setLogger(logger: Logger) {
-    this.logger = logger
-  }
+	setLogger(logger: Logger) {
+		this.logger = logger;
+	}
 
-  setupViteServer(server: ViteDevServer) {
-    if (this._server === server)
-      return
+	setupViteServer(server: ViteDevServer) {
+		if (this._server === server) return;
 
-    this._server = server
-    this.setupWatcher(server.watcher)
-  }
+		this._server = server;
+		this.setupWatcher(server.watcher);
+	}
 
-  setupWatcher(watcher: FSWatcher) {
-    watcher
-      .on('unlink', async(path) => {
-        path = slash(path)
-        if (!isTarget(path, this.options))
-          return
-        await this.removePage(path)
-        this.onUpdate()
-      })
-    watcher
-      .on('add', async(path) => {
-        path = slash(path)
-        if (!isTarget(path, this.options))
-          return
-        const page = this.options.dirs.find(i => path.startsWith(slash(resolve(this.root, i.dir))))!
-        await this.addPage(path, page)
-        this.onUpdate()
-      })
+	setupWatcher(watcher: FSWatcher) {
+		watcher.on('unlink', async (path) => {
+			path = slash(path);
+			if (!isTarget(path, this.options)) return;
+			await this.removePage(path);
+			this.onUpdate();
+		});
+		watcher.on('add', async (path) => {
+			path = slash(path);
+			if (!isTarget(path, this.options)) return;
+			const page = this.options.dirs.find((i) =>
+				path.startsWith(slash(resolve(this.root, i.dir)))
+			)!;
+			await this.addPage(path, page);
+			this.onUpdate();
+		});
 
-    watcher
-      .on('change', async(path) => {
-        path = slash(path)
-        if (!isTarget(path, this.options))
-          return
-        const page = this._pageRouteMap.get(path)
-        if (page)
-          await this.options.resolver.hmr?.changed?.(this, path)
-      })
-  }
+		watcher.on('change', async (path) => {
+			path = slash(path);
+			if (!isTarget(path, this.options)) return;
+			const page = this._pageRouteMap.get(path);
+			if (page) await this.options.resolver.hmr?.changed?.(this, path);
+		});
+	}
 
-  async addPage(path: string | string[], pageDir: PageOptions) {
-    debug.pages('add', path)
-    for (const p of toArray(path)) {
-      const pageDirPath = slash(resolve(this.root, pageDir.dir))
-      const extension = this.options.extensions.find(ext => p.endsWith(`.${ext}`))
-      if (!extension) continue
+	async addPage(path: string | string[], pageDir: PageOptions) {
+		debug.pages('add', path);
+		for (const p of toArray(path)) {
+			const pageDirPath = slash(resolve(this.root, pageDir.dir));
+			const extension = this.options.extensions.find((ext) =>
+				p.endsWith(`.${ext}`)
+			);
+			if (!extension) continue;
 
-      const route = slash(join(pageDir.baseRoute, p.replace(`${pageDirPath}/`, '').replace(`.${extension}`, '')))
-      this._pageRouteMap.set(p, {
-        path: p,
-        route,
-      })
-      await this.options.resolver.hmr?.added?.(this, p)
-    }
-  }
+			const route = slash(
+				join(
+					pageDir.baseRoute,
+					p
+						.replace(`${pageDirPath}/`, '')
+						.replace(`.${extension}`, '')
+				)
+			);
+			this._pageRouteMap.set(p, {
+				path: p,
+				route,
+			});
+			await this.options.resolver.hmr?.added?.(this, p);
+		}
+	}
 
-  async removePage(path: string) {
-    debug.pages('remove', path)
-    this._pageRouteMap.delete(path)
-    await this.options.resolver.hmr?.removed?.(this, path)
-  }
+	async removePage(path: string) {
+		debug.pages('remove', path);
+		this._pageRouteMap.delete(path);
+		await this.options.resolver.hmr?.removed?.(this, path);
+	}
 
-  onUpdate() {
-    if (!this._server)
-      return
+	onUpdate() {
+		if (!this._server) return;
 
-    invalidatePagesModule(this._server)
-    debug.hmr('Reload generated pages.')
-    this._server.ws.send({
-      type: 'full-reload',
-    })
-  }
+		invalidatePagesModule(this._server);
+		debug.hmr('Reload generated pages.');
+		this._server.ws.send({
+			type: 'full-reload',
+		});
+	}
 
-  async resolveRoutes() {
-    return this.options.resolver.resolveRoutes(this)
-  }
+	async resolveRoutes() {
+		return this.options.resolver.resolveRoutes(this);
+	}
 
-  async searchGlob() {
-    const pageDirFiles = this.options.dirs.map((page) => {
-      const pagesDirPath = slash(resolve(this.options.root, page.dir))
-      const files = getPageFiles(pagesDirPath, this.options, page)
-      debug.search(page.dir, files)
-      return {
-        ...page,
-        files: files.map(file => slash(file)),
-      }
-    })
+	async searchGlob() {
+		for await (const page of this.options.dirs) {
+			const pagesDirPath = slash(resolve(this.options.root, page.dir));
+			const files = getPageFiles(pagesDirPath, this.options, page);
+			debug.search(page.dir, files);
 
-    for (const page of pageDirFiles)
-      await this.addPage(page.files, page)
+			await this.addPage(
+				files.map((file) => slash(file)),
+				page
+			);
+		}
+		/* await this.options.dirs.forEach(async (page) => {
+			const pagesDirPath = slash(resolve(this.options.root, page.dir));
+			const files = getPageFiles(pagesDirPath, this.options, page);
+			debug.search(page.dir, files);
 
-    debug.cache(this.pageRouteMap)
-  }
+			await this.addPage(
+				files.map((file) => slash(file)),
+				page
+			);
+		}); */
 
-  get debug() {
-    return debug
-  }
+		debug.cache(this.pageRouteMap);
+	}
 
-  get pageRouteMap() {
-    return this._pageRouteMap
-  }
+	get debug() {
+		return debug;
+	}
+
+	get pageRouteMap() {
+		return this._pageRouteMap;
+	}
 }
